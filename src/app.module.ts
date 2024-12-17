e76edc78a6a9cea1f2baedb73a547ca3753b330e
NascimentoTalesDev <nascimentotalesdev@gmail.com>
2024-12-17 00:05:12 -0300

    feat: add quizzes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { QuizzesModule } from './quizzes/quizzes.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, QuizzesModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
---
+++ src/quizzes/quizzes.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { QuizzesRepository } from './quizzes.repository';
+import { CreateQuizDto } from './dto/create-quiz.dto';
+import { UpdateQuizDto } from './dto/update-quiz.dto';
+
+@Injectable()
+export class QuizzesService {
+  constructor(private quizzesRepository: QuizzesRepository) {}
+
+  async findLatestQuizzesAdded() {
+    return await this.quizzesRepository.findLatestQuizzesAdded();
+  }
+
+  async findAllFavorites(userId: number) {
+    return await this.quizzesRepository.findAllFavorites(userId);
+  }
+
+  async favorite(quizId: number, userId: number) {
+    return  await this.quizzesRepository.favorite(quizId, userId);
+  }
+
+  async like(quizId: number, userId: number) {
+    return await this.quizzesRepository.like(quizId, userId);
+  }
+
+  async unLike(quizId: number, userId: number) {
+    return await this.quizzesRepository.unLike(quizId, userId);
+  }
+  async create(createQuizDto: CreateQuizDto) {        
+    return await this.quizzesRepository.create(createQuizDto);
+  }
+
+  async findAllAdmin() {
+    return await this.quizzesRepository.findAllAdmin();
+  }
+  async findAll() {
+    return await this.quizzesRepository.findAll();
+  }
+
+  async findOne(id: number) {
+    return await this.quizzesRepository.findOne(id);
+  }
+
+  async update(id: number, updateQuizDto: UpdateQuizDto) { 
+
+    return `This action update a #${id} quiz`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} quiz`;
+  }
+}
