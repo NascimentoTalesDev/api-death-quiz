22b515468e54de3786c3100b1182eee83e110498
NascimentoTalesDev <nascimentotalesdev@gmail.com>
2024-12-16 23:54:39 -0300

    feat: initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { QuizzesModule } from './quizzes/quizzes.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, QuizzesModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
